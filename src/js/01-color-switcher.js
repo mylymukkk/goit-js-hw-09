ca0d55060c35cdfee3a4ea688c336d3bb6ace92c
Mylymuk Oksana <oksana.mylymukk@gmail.com>
2022-09-15 19:48:55 +0300

    finished task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const body = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timerId = null;
 
+startBtn.addEventListener('click', onStartBtn);
+stopBtn.addEventListener('click', onStopBtn);
+
+function onStartBtn() {
+  startBtn.setAttribute('disabled', 'disabled');
+  stopBtn.removeAttribute('disabled');
+
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+    console.log('nfdfn');
+  }, 1000);
+}
+
+function onStopBtn() {
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled', 'disabled');
+  clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
